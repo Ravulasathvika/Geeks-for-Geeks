3a2a3f5393a9f30db4d90feb6640aeb219b6da2d
Ravulasathvika <168401968+Ravulasathvika@users.noreply.github.com>
2025-04-19 20:19:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int d = Integer.parseInt(read.readLine().trim());
+
+            Solution ob = new Solution();
+            ArrayList<Integer> result = ob.subarraySum(nums, d);
+            // Print all elements in the result list
+            for (int i : result) {
+                System.out.print(i + " ");
+            }
+            System.out.println(); // Print a new line after the result
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        // code here
+        int start=0;
+        int currsum=0;
+        for(int end=0;end<arr.length;end++)
+        {
+            currsum+=arr[end];
+            while(currsum>target && start<end)
+            {
+                currsum-=arr[start];
+                start++;
+            }
+            if(currsum==target)
+            {
+                return new ArrayList<>(Arrays.asList(start+1,end+1));
+            }
+        }
+        return new ArrayList<>(Arrays.asList(-1));
+        
+    }
+}
+
