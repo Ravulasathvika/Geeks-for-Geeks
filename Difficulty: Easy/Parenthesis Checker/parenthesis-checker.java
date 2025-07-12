43e70274fbeb47c87bf0b24ca08e0f49c1bc4703
Ravulasathvika <168401968+Ravulasathvika@users.noreply.github.com>
2025-07-12 23:32:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,64 +1,33 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Driverclass {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-
-        // Reading total number of testcases
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            // reading the string
-            String st = sc.next();
-
-            // calling ispar method of Paranthesis class
-            // and printing "balanced" if it returns true
-            // else printing "not balanced"
-            if (new Solution().isBalanced(st) == true)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 
 class Solution {
     static boolean isBalanced(String s) {
         // code here
-        Stack<Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++)
+        Stack<Character> res=new Stack<>();
+        for(char c:s.toCharArray())
         {
-            char c=s.charAt(i);
-            if(c=='(' || c=='[' || c=='{')
+            if(c=='[' || c=='{' || c=='(')
             {
-                stack.push(c);
+                res.push(c);
+                
             }
             else
             {
-                if(stack.isEmpty())
+                if(res.isEmpty()) return false;
+                char top=res.pop();
+                if((top!='(' && c==')') || (top!='[' && c==']') || (top!='{' && c=='}'))
                 {
                     return false;
                 }
-                char top=stack.pop();
-                if((c==')' && top!='(') || (c==']' && top!='[') || (c=='}' && top!='{'))
-                {
-                    return false;
-                }
+                
             }
-            
         }
-        if(stack.isEmpty())
+        if(res.isEmpty())
         {
             return true;
         }
-        return false;
+        else
+        {
+            return false;
+        }
     }
 }
