36498cc5fae3a6babfa06fac94fd2928ffa2c3ae
Ravulasathvika <168401968+Ravulasathvika@users.noreply.github.com>
2025-08-16 23:07:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        int largest=-1;
+        int second=-1;
+        for(int num:arr)
+        {
+            if(num>largest)
+            {
+                second=largest;
+                largest=num;
+                
+            }
+            else if(num<largest && num>second)
+            {
+                second=num;
+            }
+            
+        }
+        return second;
+    }
+}
