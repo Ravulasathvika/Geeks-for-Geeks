8adac24af2941eefc3dddb3b98c4b1efb6c1b5e2
Ravulasathvika <168401968+Ravulasathvika@users.noreply.github.com>
2026-03-08 19:49:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of AP series/sum-of-ap-series.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+
+class Solution {
+    public long sum_of_ap(long n, long a, long d) {
+        // Code here
+        long sum;
+        sum=(n*(2*a+(n-1)*d))/2;
+        return  sum;
+          
+    }
+}
